c5b9fc07f9920fed31600f75e1f2365ca2bcfb74
Oliver Xu <oxu@u.rochester.edu>
2019-12-11 20:57:50 -0500

    safari fix

--- index.js
+++ index.js
@@ -217,7 +217,7 @@ function createLine(pattern) {
 	});
 
 	var Soundfont = require('soundfont-player');
-	var ac = new AudioContext();
+	var ac = new AudioContext() || new webkitAudioContext();
 	var instrument;
 	Soundfont.instrument(ac, '', {nameToUrl: getUrl}).then(function (bongos) {
 		instrument = bongos;
